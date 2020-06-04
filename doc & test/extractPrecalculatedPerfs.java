88d70f779fcacb102d5599db619584f0fdcaf195
supermastermind <31574054+supermastermind@users.noreply.github.com>
2020-06-05 00:48:25 +0200

    Add files via upload

--- doc & test/extractPrecalculatedPerfs.java
+++ doc & test/extractPrecalculatedPerfs.java
@@ -22,9 +22,10 @@ public class extractPrecalculatedPerfs {
   private static String[] file_table =
     {
       "STAGE2 - RESULTS_300_to_1300_3.5sec_11111 (OK).txt",
-      "STAGE1 - 1 of 5 Precalculated perfs in range 300..1300 and 3.5 seconds for 11112 and 11122 first codes.txt",
+      "STAGE3 - 1 of 5 Precalculated perfs in range 300..1300 and 3.5 seconds for 11112 first codes.txt",
+      "STAGE3 - RESULTS_270_to_2222_2.7sec_11122 (OK).txt",
       "STAGE2 - RESULTS_270_to_1700_2.7sec_11123 (OK).txt",
-      "STAGE2 - RESULTS_270_to_1700_2.7sec_11223 (OK).txt",
+      "STAGE3 - RESULTS_270_to_2222_2.7sec_11223 (OK).txt",
       "STAGE2 - RESULTS_270_to_1700_2.7sec_11234 (OK).txt",
       "STAGE2 - RESULTS_270_to_1700_2.7sec_12345 (OK).txt"
     };
--- doc & test/extract_precalculated_perfs_specific.bat
+++ doc & test/extract_precalculated_perfs_specific.bat
@@ -1,3 +1,3 @@
 @echo OFF
 
-findstr "\"2" "2 of 5 Precalculated perfs in range 270..1500 and 2.7 seconds for 11123 first code.txt"
+findstr "\"2" "STAGE1 - 1 of 5 Precalculated perfs in range 300..1300 and 3.5 seconds for 11112 and 11122 first codes.txt"

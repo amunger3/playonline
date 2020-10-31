db4a6517ebe39b92f290e51f80787d06b6e65ce9
supermastermind <31574054+supermastermind@users.noreply.github.com>
2020-11-01 00:39:54 +0100

    Add files via upload

--- SuperMasterMind.js
+++ SuperMasterMind.js
@@ -1345,7 +1345,7 @@ let play_store_app_str=
 "<center><table style='width:"+rulesTableWidthStr+";'><tr style='text-align:center;'><td><font style='font-size:1.75vh;color:black'>\
 <br><b>"+str+"</b><br>\
 <a href='"+android_app_url+"'><img alt='Get it on Google Play' style='height:11vh;margin-top:1.5vh;margin-bottom:1.5vh' src='https://play.google.com/intl/en_us/badges/images/generic/en_badge_web_generic.png'/><img alt='Get it on Google Play' style='height:11vh;margin-top:1.5vh;margin-bottom:1.5vh' src='img/Playstore_icon.png'/><br></a>\
-<small>If you like this game,&nbsp;put some stars in Google Play &#x1F609;<br>For any suggestions for improvement,&nbsp;please use the&nbsp;<a href='contact_info.html'>contact info</a> page</small><br><br><br>\
+<small>"+((specific_str.indexOf("put some stars")==-1) ? "If you like this game,&nbsp;put some stars in Google Play &#x1F609;<br>" : "")+"For any suggestions for improvement,&nbsp;please use the&nbsp;<a href='contact_info.html'>contact info</a> page</small><br><br><br>\
 </font></td></tr></table></center>";
 try{
 modal_mode=4;

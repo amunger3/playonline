6114d70e2091d6c023e5f62aa91a9681ac83f53d
supermastermind <31574054+supermastermind@users.noreply.github.com>
2020-11-27 00:43:09 +0100

    Add files via upload

--- doc & test/GameSolver_FOR_INCREMENTAL_PRECALCULATION.js
+++ doc & test/GameSolver_FOR_INCREMENTAL_PRECALCULATION.js
@@ -2557,7 +2557,9 @@ try {
                                 && (next_cur_game_idx <= maxDepthForGamePrecalculation) // (-1 or 3)
                                 && ( (next_cur_game_idx <= 1)
                                      || (next_cur_game_idx == 2)
-                                     || ((next_cur_game_idx == 3) && (nbCodes >= 450)) )
+                                     || (false /* (this new depth-3 version is skipped as very long - previous results are kept) */
+                                         && (next_cur_game_idx == 3) && (nbCodes >= 450))
+                                   )
                                 && (!compute_sum_ini) ); // not a leaf
     let str; // (precalculation mode)
     let precalculation_start_time; // (precalculation mode)

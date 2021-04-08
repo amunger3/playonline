8e636b1a17365f836a56f9bfe320fef5a3f82cbd
supermastermind <31574054+supermastermind@users.noreply.github.com>
2021-04-08 13:06:55 +0200

    Add files via upload

--- SuperMasterMind.js
+++ SuperMasterMind.js
@@ -1311,13 +1311,13 @@ show_play_store_app("<font color=#C900A1>Hi "+localStorage.firstname+"</font><hr
 show_play_store_app("<font color=#C900A1>Hi "+localStorage.firstname+"</font><hr style='height:1.0vh;padding:0;margin:0;visibility:hidden;'>If you want to merge your smartphone account & your computer account (to share the same scores on all devices), just send an email using the&nbsp;<a href='contact_info.html'>contact info</a> page", true);
 } */
 else if( localStorage.firstname&&localStorage.gamesok&&(Number(localStorage.gamesok) >=103)
-&&localStorage.lastDonationTimeT&&((new Date()).getTime() - localStorage.lastDonationTimeT > 1.11*31*24*60*60*1000 /* (~1 month) */) ){
+&&localStorage.lastDonationTimeT&&((new Date()).getTime() - localStorage.lastDonationTimeT > 1.50*31*24*60*60*1000 /* (~1.5 month) */) ){
 let paypalStr=
 "Thanks for using this "+(! android_appli? "Super Master Mind game" : "Android app")+".<hr style='height:0.25vh;padding:0;margin:0;visibility:hidden;'>You can show your appreciation and support its&nbsp;development&nbsp;by&nbsp;donating<br>\
 <hr style='height:0.25vh;padding:0;margin:0;visibility:hidden;'>\
 <a href='https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=F9EE2A483RT9J&source=url'>\
 <img alt='Donate with Paypal' style='height:6vh;margin-top:1.0vh;margin-bottom:1.0vh' src='img/paypal-donate-button.png'></a><br>\
-<small><small><font color=#AAAAAA>This message will be displayed every month</font></small></small><br>";
+<small><small><font color=#AAAAAA>This message will be displayed regularly</font></small></small><br>";
 show_play_store_app("", false, "<font color=#C900A1>Hi "+localStorage.firstname+"</font><hr style='height:1.0vh;padding:0;margin:0;visibility:hidden;'>"+paypalStr);
 localStorage.lastDonationTimeT=(new Date()).getTime();
 if(!localStorage.nbDonationRequestsT){
